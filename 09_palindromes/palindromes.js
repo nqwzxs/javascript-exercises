23d7d006819846285e113475757ee1fa5864d6b2
nqwzxs <nqwzxs@gmail.com>
2023-03-20 15:11:52 +0500

    Add solution to 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z]/g, "");
+    return string.split("").reverse().join("") === string;
 };
 
 // Do not edit below this line
