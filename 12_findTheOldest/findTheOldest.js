07da59cd7f16c558db01e0bd028b3b54d44dfff7
nqwzxs <nqwzxs@gmail.com>
2023-03-20 15:58:25 +0500

    Add solution to 11_getTheTitles

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  people.reduce(oldest, person => {
+    if (!oldest) return person;
+    ageOfPerson = person.yearOfBirth - person.yearOfDeath;
+    ageOfOldest = oldest.yearOfBirth - oldest.yearOfDeath;
+    return ageOfOldest > ageOfPerson ? oldest : person
+  });
 };
 
 // Do not edit below this line
